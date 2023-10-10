43e0429a81c4255ef2236743b073601c88df71d0
syunt <syunt@DES>
2023-10-10 12:53:24 +0900

    元

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
 ﻿#include<stdio.h>
 
-template<typename T>
-
-T add(T a, T b) {
-	return a+b;
+int Recursive(int n) {
+	if (n <= 1)
+	{
+		return(1);
+	}
+	return(n * Recursive(n - 1));
 }
+int main() {
+	int n = 10;
+	int result;
 
-template<>
-float add<float>(float a, float b) {//オーバーロード特殊化
-	return a - b;
-}
+	result = Recursive(n);
+	printf("%dの階乗=%d\n", n, result);
 
-float add(float a, float b){//オーバーロード多重定義
-	return a * b;
-}
-
-int main(void) {
-	printf("%d\n",add<int>(114,514));//628 関数テンプレートを利用
-	printf("%f\n", add<float>(11.4f, 51.4f));//-40.0 特殊化(オーバーライド)を利用
-	printf("%f\n", add(11.4f, 51.4f));//585.96 多重定義(オーバーロード)を利用
-
-	return 0;
+	return(0);
 }
