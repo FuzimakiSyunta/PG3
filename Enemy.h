4ab94d679c0b530b81f05ec3f5798c1c2965aac8
syunt <syunt@DES>
2023-10-31 10:21:55 +0900

    途中

--- Enemy.h
+++ Enemy.h
@@ -1,6 +1,24 @@
 #pragma once
 class Enemy
 {
+public:
+	// デストラクタ
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+private:
 
 };
 
