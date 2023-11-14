7f68ba41fb4c2e0857eb40820a929d9f230f99ad
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2023-11-14 11:36:51 +0900

    途中

--- Enemy.cpp
+++ Enemy.cpp
@@ -13,21 +13,23 @@ void Enemy::Update()
 {
 	//メンバ関数ポインタに関数アドレスを代入する
 	Phase = &Enemy::Approach;
+	Phase = &Enemy::Fire;
+	Phase = &Enemy::Leave;
 }
 
 void Enemy::Approach()
 {
-
+	printf("接近\n");
 }
 
 void Enemy::Fire()
 {
-
+	printf("射撃\n");
 }
 
 void Enemy::Leave()
 {
-
+	printf("離脱\n");
 }
 
 
